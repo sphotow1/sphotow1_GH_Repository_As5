537fe6e7b30a7f241c31236f04a633305ffaa37a
sphotow1 <67553260+sphotow1@users.noreply.github.com>
2020-06-28 14:51:43 -0400

    commit 3
    jkjk

--- Solution1.java
+++ Solution1.java
@@ -9,6 +9,6 @@ import java.util.Scanner;
 public class Solution1 {
 	    
 	    public static void main(String args[]) {
-	       System.out.println("Hellow WOrlddddd ready to commit!!!");	        
+	       System.out.println("Hellow WOrlddddd ready to commit 3!!!");	        
 	    }
 	}
